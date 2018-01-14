4b652e34502eb83cf21fc8d93c1823c5d80e5b82
André Kunitz <andrekunitz@gmail.com>
2018-01-14 14:48:41 -0200

    Criado classe Produto e assicioacao muitos para muitos com a clasee Categoria

---
+++ src/main/java/com/andrekunitz/modelagem/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.andrekunitz.modelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.andrekunitz.modelagem.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
